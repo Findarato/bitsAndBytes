4bb4570324df9f6f00b05e86adc522bae39d130c
Joseph Harry <findarato@gmail.com>
2016-02-25 04:50:06 -0600

    Added the adafruit include

--- content/pi/setupSpeedtest.sh
+++ content/pi/setupSpeedtest.sh
@@ -1,6 +1,6 @@
-pip install git+https://github.com/sivel/speedtest-cli.git
-
+sudo pip install git+https://github.com/sivel/speedtest-cli.git
+sudo pip install adafruit-io
 
 #Enter one of the two into crontab. These are setup for the raspberry pi
-0 * * * *  /usr/bin/python /home/pi/python/aio.reportSpeed.py >> /home/pi/python/reportSpeed.txt 2>&1
-#*/30 * * * *  /usr/bin/python /home/pi/python/aio.reportSpeed.py >> /home/pi/python/reportSpeed.txt 2>&1
+# 0 * * * *  /usr/bin/python /home/pi/python/aio.reportSpeed.py >> /home/pi/python/reportSpeed.txt 2>&1
+# */30 * * * *  /usr/bin/python /home/pi/python/aio.reportSpeed.py >> /home/pi/python/reportSpeed.txt 2>&1
